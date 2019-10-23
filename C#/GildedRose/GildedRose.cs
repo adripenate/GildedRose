ab375482ab0967fbd5f652dbf021395459edfae2
adripsau <adrianapenate98@gmail.com>
2019-10-23 12:13:36 +0100

    Refactor - Constants of magic strings created Code smell: Excessive use of literals

--- C#/GildedRose/GildedRose.cs
+++ C#/GildedRose/GildedRose.cs
@@ -4,6 +4,10 @@ namespace GildedRose
 {
     public class GildedRose
     {
+        private const string AGEDBRIE_ITEM = "Aged Brie";
+        private const string BACKSTAGE_PASSES_ITEM = "Backstage passes to a TAFKAL80ETC concert";
+        private const string SULFURAS_ITEM = "Sulfuras, Hand of Ragnaros";
+
         IList<Item> Items;
         public GildedRose(IList<Item> Items)
         {
@@ -14,11 +18,11 @@ namespace GildedRose
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (Items[i].Name != AGEDBRIE_ITEM && Items[i].Name != BACKSTAGE_PASSES_ITEM)
                 {
                     if (Items[i].Quality > 0)
                     {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        if (Items[i].Name != SULFURAS_ITEM)
                         {
                             Items[i].Quality = Items[i].Quality - 1;
                         }
@@ -30,7 +34,7 @@ namespace GildedRose
                     {
                         Items[i].Quality = Items[i].Quality + 1;
 
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (Items[i].Name == BACKSTAGE_PASSES_ITEM)
                         {
                             if (Items[i].SellIn < 11)
                             {
@@ -51,20 +55,20 @@ namespace GildedRose
                     }
                 }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                if (Items[i].Name != SULFURAS_ITEM)
                 {
                     Items[i].SellIn = Items[i].SellIn - 1;
                 }
 
                 if (Items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (Items[i].Name != AGEDBRIE_ITEM)
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (Items[i].Name != BACKSTAGE_PASSES_ITEM)
                         {
                             if (Items[i].Quality > 0)
                             {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                if (Items[i].Name != SULFURAS_ITEM)
                                 {
                                     Items[i].Quality = Items[i].Quality - 1;
                                 }
