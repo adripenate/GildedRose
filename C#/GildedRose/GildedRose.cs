65f24f77b9d9543fa64d0d60c9d718e7e50bb635
adripsau <adrianapenate98@gmail.com>
2019-10-25 15:30:44 +0100

    Refactor - BackstagePasses implemented with CustomItem Factory

--- C#/GildedRose/GildedRose.cs
+++ C#/GildedRose/GildedRose.cs
@@ -6,19 +6,12 @@ namespace GildedRose
     public class GildedRose
     {
         private const string AgedBrieItem = "Aged Brie";
-        private const string BackstagePassesItem = "Backstage passes to a TAFKAL80ETC concert";
-        private const string SulfurasItem = "Sulfuras, Hand of Ragnaros";
-        private const int QualityLowLimit = 0;
         private const int QualityHighLimit = 50;
-        private const int SellInLimitDropQualityByTwo = 11;
-        private const int SellInLimitDropQualityByThree = 6;
-        private const int SellInLowLimit = 0;
 
         IList<Item> Items;
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
-            ItemFactory itemFactory = new ItemFactory();
         }
 
         public void UpdateQuality()
@@ -28,28 +21,9 @@ namespace GildedRose
                 CustomItem customItem = ItemFactory.CreateCustomItem(Items[i]);
                 customItem.UpdateSellIn(Items[i]);
 
-                if (!IsBackstagePasses(Items[i].Name) && !IsAgedBrie(Items[i].Name))
+                if (!IsAgedBrie(Items[i].Name))
                 {
                     customItem.UpdateQuality(Items[i]);
-                }
-                if (IsBackstagePasses(Items[i].Name))
-                {
-                    UpdateQuality(Items[i], 1);
-
-                    if (IsBellowSellInLimitDropByTwo(Items[i].SellIn) && IsBellowQualityHighLimit(Items[i].Quality))
-                    {
-                        UpdateQuality(Items[i], 1);
-                    }
-
-                    if (IsBellowSellInLimitDropByThree(Items[i].SellIn) && IsBellowQualityHighLimit(Items[i].Quality))
-                    {
-                        UpdateQuality(Items[i], 1);
-                    }
-
-                    if (IsBellowSellInLowLimit(Items[i].SellIn))
-                    {
-                        UpdateQuality(Items[i], -Items[i].Quality);
-                    }
                 }
                 else if (IsAgedBrie(Items[i].Name) && IsBellowQualityHighLimit(Items[i].Quality))
                 {
@@ -64,39 +38,9 @@ namespace GildedRose
             item.Quality += update;
         }
 
-        private bool IsBellowSellInLowLimit(int sellIn)
-        {
-            return sellIn < SellInLowLimit;
-        }
-
-        private bool IsBellowSellInLimitDropByThree(int sellIn)
-        {
-            return sellIn < SellInLimitDropQualityByThree;
-        }
-
-        private bool IsBellowSellInLimitDropByTwo(int sellIn)
-        {
-            return sellIn < SellInLimitDropQualityByTwo;
-        }
-
         private bool IsBellowQualityHighLimit(int quality)
         {
             return quality < QualityHighLimit;
-        }
-
-        private bool IsAboveQualityLowLimit(int quality)
-        {
-            return quality > QualityLowLimit;
-        }
-
-        private bool IsSulfurasItem(string itemName)
-        {
-            return itemName == SulfurasItem;
-        }
-
-        private bool IsBackstagePasses(string itemName)
-        {
-            return itemName == BackstagePassesItem;
         }
 
         private bool IsAgedBrie(string itemName)
@@ -170,6 +114,11 @@ namespace GildedRose
 
     public class BackstagePassesItem : CustomItem
     {
+        private const int QualityHighLimit = 50;
+        private const int SellInLimitDropQualityByTwo = 11;
+        private const int SellInLimitDropQualityByThree = 6;
+        private const int SellInLowLimit = 0;
+
         public void UpdateSellIn(Item item)
         {
             item.SellIn -= 1;
@@ -177,7 +126,37 @@ namespace GildedRose
 
         public void UpdateQuality(Item item)
         {
-            throw new System.NotImplementedException();
+            IncreaseQuality(item);
+
+            if (IsBellowSellInLimitDropByTwo(item.SellIn) && IsBellowQualityHighLimit(item.Quality)) IncreaseQuality(item);
+            
+            if (IsBellowSellInLimitDropByThree(item.SellIn) && IsBellowQualityHighLimit(item.Quality)) IncreaseQuality(item);
+            
+            if (IsBellowSellInLowLimit(item.SellIn)) item.Quality = 0;
+        }
+
+        private void IncreaseQuality(Item item)
+        {
+            item.Quality += 1;
+        }
+
+        private bool IsBellowQualityHighLimit(int quality)
+        {
+            return quality < QualityHighLimit;
+        }
+        private bool IsBellowSellInLowLimit(int sellIn)
+        {
+            return sellIn < SellInLowLimit;
+        }
+
+        private bool IsBellowSellInLimitDropByThree(int sellIn)
+        {
+            return sellIn < SellInLimitDropQualityByThree;
+        }
+
+        private bool IsBellowSellInLimitDropByTwo(int sellIn)
+        {
+            return sellIn < SellInLimitDropQualityByTwo;
         }
     }
 
